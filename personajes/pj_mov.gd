c6d7fc793c7a13c55b3175ecd843254b16a423ae
msspza <msapienza@fi.uba.ar>
2025-08-12 21:10:26 -0300

    Se reestructuro el proyecto y se agregaron los bocetos del personaje de 'Wary'

--- personajes/pj_mov.gd
+++ personajes/pj_mov.gd
@@ -5,11 +5,19 @@ extends CharacterBody2D
 @export var salto=300
 
 @onready var animated_skin=$SKIN
+@onready var timer_idle=$TimerIdle
+
+var timer_activo=false
+var default_animation=false
 
 func _physics_process(delta: float) -> void:
-	# Encontrar la forma de reproducir el 'default' sin alterar las otras animaciones
-	if not Input.is_anything_pressed():
-		animated_skin.play("default")
+	if not Input.is_anything_pressed() and not default_animation:
+		if not timer_activo:
+			timer_idle.start()
+			timer_activo=true
+	
+	if Input.is_anything_pressed():
+		default_animation=false
 		
 	var direccion=Input.get_axis("izquierda","derecha")
 	velocity.x=velocidad*direccion
@@ -24,11 +32,18 @@ func _physics_process(delta: float) -> void:
 		animated_skin.play("salto")
 		velocity.y=velocity.y - salto
 	
+	if not is_on_floor():
+		velocity.y=velocity.y + gravedad*delta
+		
 	var golpe_presionado=Input.is_action_just_pressed("pegar")
 	if golpe_presionado:
 		animated_skin.play("pegar")
 		
-	if not is_on_floor():
-		velocity.y=velocity.y + gravedad*delta
-	
 	move_and_slide()
+
+# Método que va a accionarse tras la señal emitida por el 'Timer'. La funcion del mismo será reproducir la animación 'default' del personaje luego de haber pasado 1s
+func _on_timer_idle_timeout() -> void:
+	animated_skin.play("default")
+	timer_activo=false
+	default_animation=true
+	timer_idle.stop()
